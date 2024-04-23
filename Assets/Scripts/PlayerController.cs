823a642ace6be9d51a11df9a6c3496b1f500c062
spencer <94473307+sparrowpaws@users.noreply.github.com>
2024-04-22 21:57:08 -0400

    working on animation
    removed an animation, changed file name, and trying new code for controller to implement movement. having issue with animator resetting, but know how to add animation, so will fix it soon

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator;
+
+    private bool isMoving;
+    private Vector2 input;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                Vector3 targetPos = transform.position + new Vector3(input.x, input.y, 0f);
+                StartCoroutine(Move(targetPos));
+
+                // Set animation parameters based on movement direction
+                animator.SetFloat("Horizontal", input.x);
+                animator.SetFloat("Vertical", input.y);
+                animator.SetBool("IsMoving", true);
+            }
+            else
+            {
+                // If not moving, play idle animation
+                animator.SetBool("IsMoving", false);
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
+
+/*   public float moveSpeed;
+
+   private bool isMoving;
+
+   private Vector2 input;
+
+
+
+   private void Update()
+   {
+       if (!isMoving)
+       {
+           input.x = Input.GetAxisRaw("Horizontal");
+           input.y = Input.GetAxisRaw("Vertical");
+
+           if (input != Vector2.zero)
+           {
+               var targetPos = transform.position;
+               targetPos.x += input.x;
+               targetPos.y += input.y;
+
+               StartCoroutine(Move(targetPos));
+           }
+       }
+   }
+
+   IEnumerator Move(Vector3 targetPos)
+   {
+       isMoving = true;
+       while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+       {
+           transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+           yield return null;
+       }
+       transform.position = targetPos;
+
+       isMoving = false;
+   }
+*/
+
